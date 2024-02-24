56e33c68fc8dc44bb8d13edcb0f1527d89405f83
kukuh2005 <kukuh2005>
2024-02-25 05:06:04 +0700

    first commit

---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg main-navbar">
+        <form class="form-inline mr-auto">
+          <ul class="navbar-nav mr-3">
+            <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+            <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li>
+          </ul>
+        </form>
+        <ul class="navbar-nav navbar-right">
+          <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+            <!-- <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle mr-1"> -->
+            <div class="d-sm-none d-lg-inline-block">{{auth()->user()->nama}}</div></a>
+            <div class="dropdown-menu dropdown-menu-right">
+              @if(auth()->user()->level=='admin')
+              <a href="/{{auth()->user()->level}}/user" class="dropdown-item has-icon">
+                <i class="fas fa-user"></i> User
+              </a>
+              <div class="dropdown-divider"></div>
+              @endif
+              <a href="/logout" class="dropdown-item has-icon text-danger">
+                <i class="fas fa-sign-out-alt"></i> Logout
+              </a>
+            </div>
+          </li>
+        </ul>
+      </nav>
