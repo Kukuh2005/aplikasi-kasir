a427d411f3bc8e21653530993959046e613e9542
Kukuh2005 <kukuhwisanggeni09@gmail.com>
2024-02-26 14:29:40 +0700

    profile page

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -10,12 +10,10 @@
             <!-- <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle mr-1"> -->
             <div class="d-sm-none d-lg-inline-block">{{auth()->user()->nama}}</div></a>
             <div class="dropdown-menu dropdown-menu-right">
-              @if(auth()->user()->level=='admin')
-              <a href="/{{auth()->user()->level}}/user" class="dropdown-item has-icon">
-                <i class="fas fa-user"></i> User
+              <a href="/{{auth()->user()->level}}/profile" class="dropdown-item has-icon">
+                <i class="fas fa-user"></i> Profile
               </a>
               <div class="dropdown-divider"></div>
-              @endif
               <a href="/logout" class="dropdown-item has-icon text-danger">
                 <i class="fas fa-sign-out-alt"></i> Logout
               </a>
