86112ff61c9fa7fb3044b01ad79d94ae44f762aa
Kukuh2005 <kukuhwisanggeni09@gmail.com>
2024-02-27 11:29:31 +0700

    foto

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -79,12 +79,12 @@
                                                 placeholder="Opsional">
                                         </div>
                                     </div>
-                                    <div class="col-md-12">
+                                    <!-- <div class="col-md-12">
                                         <div class="form-group">
                                             <label for="foto">Foto</label>
                                             <input type="file" class="form-control-file mt-2" name="foto">
                                         </div>
-                                    </div>
+                                    </div> -->
                                 </div>
                                 <div class="card-footer text-right">
                                     <button type="submit" class="btn btn-outline-primary">Simpan</button>
